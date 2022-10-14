b632b26a40519ac9db9bdb5b203d3c45e5316e00
neeraj <neerajp4321@gmail.com>
2022-10-14 22:43:11 +0530

    Feat: complete the project

--- project-headerparser/index.js
+++ project-headerparser/index.js
@@ -24,6 +24,13 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+  const ipaddress = req.connection.remoteAddress
+  const language = 'en-US'
+  const software = 'VS Code'
+  res.json({ ipaddress, language, software })
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
