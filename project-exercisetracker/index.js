b0a09049218979a20439ea639a3e3b7de11fb412
neeraj <neerajp4321@gmail.com>
2022-10-16 16:10:09 +0530

    Feat:add mongoose, schema and connect to database

--- project-exercisetracker/index.js
+++ project-exercisetracker/index.js
@@ -3,15 +3,29 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
+/* to start mongoose */
+const mongoose = require('mongoose')
+const mySecret = process.env['MONGO_URI']
+mongoose.connect(mySecret).then(() => {
+  console.log('Mongoose connected')
+})
+
+const User = require('./model.js')
+
 app.use(cors())
 app.use(express.static('public'))
+app.use(express.urlencoded({ extendend: true }))
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
-
-
+app.post('/api/users', async (req, res) => {
+  /* for adding new user to database */
+  const { username } = req.body
+  const user = await User.create({ username })
+  res.json({ username: user.username, _id: user._id })
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
