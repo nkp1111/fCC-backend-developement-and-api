c87174da3d420f227673f315a6e4c93abf2f4e58
neeraj <neerajp4321@gmail.com>
2022-10-17 12:13:01 +0530

    Feat: complete project file metadata

--- project-filemetadata/index.js
+++ project-filemetadata/index.js
@@ -1,17 +1,28 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const fs = require('fs')
+const fileUpload = require('express-fileupload')
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.urlencoded({ extended: true }))
+app.use(fileUpload())
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', (req, res) => {
+  const { upfile } = req.files
+  const name = upfile.name
+  const type = upfile.mimetype
+  const size = upfile.size
+  console.log(name, type, size)
+  res.json({ name, type, size })
+})
 
 
 const port = process.env.PORT || 3000;
